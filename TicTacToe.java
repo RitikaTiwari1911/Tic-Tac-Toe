f46094a787eae8cedf40aac0c7d37e65d86f67b8
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-06 11:24:04 +0530

    CREATED: a tic tac toe board

---
+++ TicTacToe.java
@@ -0,0 +1,17 @@
+public class TicTacToe {
+
+    public static char[] createBoard() {
+        char[] board=new char[10];
+        for(int i =1;i<board.length;i++) {
+            System.out.println(board[i]);
+        }
+        return board;
+
+    }
+
+    public static void main(String args[])
+    {
+        createBoard();
+    }
+
+}
