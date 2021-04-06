10dfa8d9ddf0fb13e0905e5b4f8659f30c68a091
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-06 12:38:38 +0530

    ADDED:playing options X and O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class TicTacToe {
 
+    //creating board
     public static char[] createBoard() {
-        char[] board=new char[10];
-        for(int i =1;i<board.length;i++) {
+        char[] board = new char[10];
+        for (int i = 1; i < board.length; i++) {
             System.out.println(board[i]);
         }
         return board;
 
     }
 
+    //providing playing options
+    public static void chooseOption() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Select X or O:");
+        String userChoice=input.next();
+        System.out.println("User choice:"+userChoice);
+        
+        if (userChoice.equalsIgnoreCase("x"))
+            System.out.println("Computer choice:O");
+        else if (userChoice.equalsIgnoreCase("o"))
+            System.out.println("Computer choice:X");
+        else
+            System.out.println("Enter the correct option!");
+    }
+
     public static void main(String args[])
     {
         createBoard();
+        chooseOption();
     }
 
 }
